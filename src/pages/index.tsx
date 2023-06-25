9d9ac7dd0bb16016ce9f0e1f20fd6b8ec1ba2292
zeinabsayyadi <zeinsa0158@gmail.com>
2023-06-25 21:51:21 +0430

    fix: home buttons style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,10 +14,10 @@ export default function Home() {
     >
       <h1>welcome to our project</h1>
       <div>
-        <button>
+        <button className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center m2">
           <Link href="./auth/signin">signin</Link>
         </button>
-        <button>
+        <button className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center m-2">
           <Link href="./auth/signup">signup</Link>
         </button>
       </div>
